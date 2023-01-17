d3c95965d467f38c56d15fef33cd3b5759d05b9c
Paulette Del Valle <paulette.dvm@gmail.com>
2023-01-17 13:48:55 -0500

    ok, i think i fixed the issue w/ the circle numbers. now have to double check that it works properly in max/min times.

--- sketch.js
+++ sketch.js
@@ -1,46 +1,49 @@
+let x = 25, y = 25; 
+
 let sec_total = 60, 
     min_total = 60, 
     hr_total = 24; 
 
-let xy_hr = [25, 25]
-
-function setup() {
+function setup() { 
   createCanvas(800, 600);
 }
 
 function draw() {
-  background(0);
+  background(0); 
   
   let hr_col = color(252, 147, 26);  
-  let xy_min = draw_circles(hr_total, hour(), hr_col, xy_hr[0], xy_hr[1]);
-  
-  let min_col = color(24, 157, 194);
-  let xy_sec = draw_circles(min_total, minute(), min_col, xy_min[0], xy_min[1]) 
-  
- let sec_col = color(252, 232, 26); 
- draw_circles(sec_total, second(), sec_col, xy_sec[0], xy_sec[1]);
+  xy_min = draw_circles(hr_total, hour(), hr_col, 25, 25);
 
-    console.log(hour(), minute(), second()); 
+  let min_col = color(24, 157, 194);
+  let xy_sec = draw_circles(min_total, minute(), min_col, xy_min[0], xy_min[1]); 
+
+  let sec_col = color(252, 232, 26); 
+  draw_circles(sec_total, second(), sec_col, xy_sec[0], xy_sec[1]); 
+  
 }
 
-function draw_circles(total, number, color, x, y) {
-  for (let i = 0; i < total; i++) {
-    if (i <= number) {
-      //console.log("here")
-      fill(color); 
-    } else {
+function draw_circles(total, curr, color, x, y) {
+  for(let i = 1; i <= total; i++) {
+
+    if (i <= curr) {
+      fill(color);
+    } else { 
       fill(255);
     }
-  
-    if (x <= width && y <= height) {
-      ellipse(x, y, 25, 25); 
-      x += 50; 
-    } else if (x > width && y <= height) {
-      x = 25;
-      y += 50; 
+
+    // create elipse
+    ellipse(x, y, 25, 25);  
+
+    // adjust the coordinates for the next elipse
+    if (x < 775) { 
+      // 775 is the max possible dimension the ellipse can be
+      x += 50;
+    } else { 
+      // so if the x is > or = to 755 that means that the next ellipse needs to be in a new row
+      x = 25; 
+      y += 50;
     }
-    
   }
-  
+
   return [x, y];
 }
